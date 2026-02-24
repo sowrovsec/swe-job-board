ccff984921b94a63e2d8d0818318a09b97e9bff7
sowrovsec <dev.sowrovsarkar63@gmail.com>
2026-02-24 22:40:45 +0600

    extracted dom element from html file

--- js/job-tracker.js
+++ js/job-tracker.js
@@ -4,3 +4,25 @@ let allList = [
     { company: "HealthSync", position: "iOS Developer", details: "London, UK • Contract • $90,000", desc: "Build and maintain healthcare tracking applications.", status: "Not Applied" },
     { company: "FinTech Dynamics", position: "Blockchain Developer", details: "Miami, FL • Full-time • $160,000", desc: "Design smart contracts for financial services.", status: "Not Applied" }
 ];
+
+let interviewList = [];
+let rejectedList = [];
+
+let activeTab = 'All'; 
+
+// get all element from html file
+const totalCount = document.getElementById('totalCount');
+const interviewCount = document.getElementById('interviewCount');
+const rejectedCount = document.getElementById('rejectedCount');
+const availableCount = document.getElementById('availableCount');
+const cardsSection = document.getElementById('cardsSection');
+const emptyState = document.getElementById('emptyState');
+const mainContainer = document.getElementById('mainContainer');
+
+        // counter function 
+function calculateCount() {
+    totalCount.innerText = allList.length + interviewList.length + rejectedList.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
+
